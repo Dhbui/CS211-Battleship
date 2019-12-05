1160e736e0f4ff9cf2a8fa1ca8fa7d5bf558d1e4
Janny787 <39030224+Janny787@users.noreply.github.com>
2019-12-05 13:20:45 -0500

    Add files via upload

---
+++ Window.java
@@ -0,0 +1,30 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+//Adds the frame and all that jazz, starts the game
+public class Window extends Canvas {
+	
+	public Window(int width, int height, String name, BattleshipGame game) {
+		
+		JFrame f = new JFrame(name);
+		
+		f.setPreferredSize(new Dimension(width, height));
+		f.setMaximumSize(new Dimension(width, height));
+		f.setMinimumSize(new Dimension(width, height));
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
+		f.add(game);   
+		f.setVisible(true);
+		game.setupGame();
+		game.runGame();
+	}
+}
