908e1cb350c0820dad57e22ca38eeaeee1cb4433
Dylan Bui <dhbui16@gmail.com>
2019-12-05 01:32:59 -0500

    Added handler for multiplayer games.

---
+++ src/MultiPlayerHandler.java
@@ -0,0 +1,11 @@
+/**
+ * A Handler class to handle the creation and running of the BattleshipGame Object for 2 players.
+ * @author Dylan Bui
+ */
+public class MultiPlayerHandler {
+    public static void main(String[] args) {
+        BattleshipGame game = new BattleshipGame("");
+        game.setupGame();
+        game.runGame();
+    }
+}
